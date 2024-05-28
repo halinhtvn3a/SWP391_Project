baab397c9e1531e68e1fe7cf4b236e88676ab51e
ch1kkgq <truongtanhuy3006@gmail.com>
2024-05-28 19:05:56 +0700

    Identity
    Identity

---
+++ IdentityAndMVC/BusinessObjects/Review.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace BusinessObjects
+{
+	public class Review
+	{
+		[Key]
+		[StringLength(5)]
+		public string ReviewId { get; set; }
+
+		[StringLength(255)]
+		public string ReviewText { get; set; }
+
+		public DateTime? ReviewDate { get; set; }
+
+		public int? Rating { get; set; }
+
+		[ForeignKey("User")]
+		[StringLength(450)]
+		public string Id { get; set; }
+
+		[ForeignKey("Court")]
+		[StringLength(5)]
+		public string CourtId { get; set; }
+
+		// Navigation properties
+		public IdentityUser User { get; set; }
+		public Court Court { get; set; }
+	}
+}
---
+++ IdentityAndMVC/BusinessObjects/UserDetail.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace BusinessObjects
+{
+	public class UserDetail
+	{
+		[Key]
+		[StringLength(5)]
+		public string UserDetailsId { get; set; }
+
+		public decimal? Balance { get; set; }
+
+		[StringLength(50)]
+		public string FullName { get; set; }
+
+		[Required]
+		public bool Status { get; set; }
+
+		// Navigation property
+		public IdentityUser User { get; set; }
+	}
+}
