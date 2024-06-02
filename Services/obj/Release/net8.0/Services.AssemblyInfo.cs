38462a8d4941ac12da93f8c8f1187ded2f052903
halinhtvn3a <halinh150@gmail.com>
2024-06-02 20:27:49 +0700

    deploy v1.2

--- Services/obj/Release/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Release/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec5e84c55230dba15a9f564c2bfdde444449de25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e1c92fe2b79be44aa057a0ff25aae1611315cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
