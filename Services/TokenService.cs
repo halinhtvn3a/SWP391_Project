16392ef1118b6c9b7378a4ba97b97c328dadb577
halinhtvn3a <halinh150@gmail.com>
2024-06-24 00:01:31 +0700

    fix token and test

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -2,13 +2,11 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
+
 using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 using System.Text;
-using System.Threading.Tasks;
+
 using Services.Interface;
 
 namespace Services
