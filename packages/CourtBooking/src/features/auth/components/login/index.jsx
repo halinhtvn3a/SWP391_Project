875b5bf0229c188c77da393b8b854681ee04c605
LeThanhNhan91 <lethanhnhan09012004@gmail.com>
2025-07-15 16:39:59 +0700

    editing import path and display log in UI

--- packages/CourtBooking/src/features/auth/components/login/index.jsx
+++ packages/CourtBooking/src/features/auth/components/login/index.jsx
@@ -1,25 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import "./loginTest.scss";
+// import "./loginTest.scss";
 import { FaGoogle, FaFacebookF } from "react-icons/fa";
-import { loginApi } from "api/usersApi";
+// import { loginApi } from "../../../user/api/usersApi";
 import {
   validateFullName,
   validateEmail,
   validatePassword,
   validateConfirmPassword,
-} from "../Validations/formValidation";
-import axios from "axios";
+} from "../../../../components/shared/Validations/formValidation";
+// import axios from "axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { GoogleLogin } from "@react-oauth/google";
 import ClipLoader from "react-spinners/ClipLoader";
-import { ROUTERS } from "utils/router";
+// import { ROUTERS } from "utils/router";
 import { FacebookAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "firebase.js";
-import { useAuth } from "AuthContext.js";
+import { auth } from "../../../../firebase.js";
+// import { useAuth } from "../../AuthContext.jsx";
 import { jwtDecode } from "jwt-decode";
-import { fetchUserById, fetchRoleByUserId } from "api/userApi";
+// import { fetchUserById, fetchRoleByUserId } from "api/userApi";
 
 const Login = () => {
   const [password, setPassword] = useState("");
@@ -49,8 +48,7 @@ const Login = () => {
     message: "",
   });
 
-  const navigate = useNavigate();
-  const { login } = useAuth();
+  // const { login } = useAuth();
 
   useEffect(() => {
     const container = document.getElementById("container");
@@ -73,111 +71,111 @@ const Login = () => {
     };
   }, []);
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+  // const handleLogin = async (e) => {
+  //   e.preventDefault();
 
-    if (!email || !password) {
-      toast.error("Email/Password is required!");
-      return;
-    }
+  //   if (!email || !password) {
+  //     toast.error("Email/Password is required!");
+  //     return;
+  //   }
 
-    setLoading(true);
-    try {
-      const res = await loginApi(email, password);
-      if (res && res.token) {
-        localStorage.setItem("token", res.token);
-        console.log("token: ", res.token);
-        var decode = jwtDecode(res.token);
-        localStorage.setItem("userRole", decode.role);
-        const userData = {
-          email: decode.email,
-          role: decode.role,
-        };
-        login(userData); // Lưu thông tin người dùng vào context
-        navigate(ROUTERS.USER.HOME);
-      } else if (res && res.status === 401) {
-        setMessage("Login failed!");
-        setMessageType("error");
-      } else if (
-        res &&
-        res.data.status === "Error" &&
-        res.data.message == "User is banned!"
-      ) {
-        toast.error("This account is banned!", {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "colored",
-        });
-        return;
-      }
-    } catch (error) {
-      setMessage("Login failed!");
-      setMessageType("error");
-    } finally {
-      setLoading(false);
-    }
-  };
+  //   setLoading(true);
+  //   try {
+  //     const res = await loginApi(email, password);
+  //     if (res && res.token) {
+  //       localStorage.setItem("token", res.token);
+  //       console.log("token: ", res.token);
+  //       var decode = jwtDecode(res.token);
+  //       localStorage.setItem("userRole", decode.role);
+  //       const userData = {
+  //         email: decode.email,
+  //         role: decode.role,
+  //       };
+  //       login(userData); // Lưu thông tin người dùng vào context
+  //       // navigate(ROUTERS.USER.HOME);
+  //     } else if (res && res.status === 401) {
+  //       setMessage("Login failed!");
+  //       setMessageType("error");
+  //     } else if (
+  //       res &&
+  //       res.data.status === "Error" &&
+  //       res.data.message == "User is banned!"
+  //     ) {
+  //       toast.error("This account is banned!", {
+  //         position: "top-right",
+  //         autoClose: 5000,
+  //         hideProgressBar: false,
+  //         closeOnClick: true,
+  //         pauseOnHover: true,
+  //         draggable: true,
+  //         progress: undefined,
+  //         theme: "colored",
+  //       });
+  //       return;
+  //     }
+  //   } catch (error) {
+  //     setMessage("Login failed!");
+  //     setMessageType("error");
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
 
-  const handleRegister = async (e) => {
-    e.preventDefault();
+  // const handleRegister = async (e) => {
+  //   e.preventDefault();
 
-    const fullNameValidation = validateFullName(fullName);
-    const emailValidation = await validateEmail(email);
-    const passwordValidation = validatePassword(password);
-    const confirmPasswordValidation = validateConfirmPassword(
-      password,
-      confirmPassword
-    );
+  //   const fullNameValidation = validateFullName(fullName);
+  //   const emailValidation = await validateEmail(email);
+  //   const passwordValidation = validatePassword(password);
+  //   const confirmPasswordValidation = validateConfirmPassword(
+  //     password,
+  //     confirmPassword
+  //   );
 
-    setFullNameValidation(fullNameValidation);
-    setEmailValidation(emailValidation);
-    setPasswordValidation(passwordValidation);
-    setConfirmPasswordValidation(confirmPasswordValidation);
+  //   setFullNameValidation(fullNameValidation);
+  //   setEmailValidation(emailValidation);
+  //   setPasswordValidation(passwordValidation);
+  //   setConfirmPasswordValidation(confirmPasswordValidation);
 
-    if (
-      !fullNameValidation.isValid ||
-      !emailValidation.isValid ||
-      !passwordValidation.isValid ||
-      !confirmPasswordValidation.isValid
-    ) {
-      setMessage("Please try again");
-      setMessageType("error");
-      return;
-    }
+  //   if (
+  //     !fullNameValidation.isValid ||
+  //     !emailValidation.isValid ||
+  //     !passwordValidation.isValid ||
+  //     !confirmPasswordValidation.isValid
+  //   ) {
+  //     setMessage("Please try again");
+  //     setMessageType("error");
+  //     return;
+  //   }
 
-    setLoading(true);
+  //   setLoading(true);
 
-    try {
-      const response = await axios.post(
-        "https://courtcaller.azurewebsites.net/api/authentication/register",
-        {
-          fullName: fullName,
-          email: email,
-          password: password,
-          confirmPassword: confirmPassword,
-        }
-      );
-      toast.success("Registration successful!");
-      setMessage("SIGN UP SUCCESSFULLY - LOG IN NOW");
-      setMessageType("success");
-      setIsLogin(true);
-    } catch (error) {
-      if (error.response) {
-        toast.error(error.response.data.message || "Registration failed");
-      } else if (error.request) {
-        toast.error("No response from server");
-      } else {
-        toast.error(error.message);
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
+  //   try {
+  //     const response = await axios.post(
+  //       "https://courtcaller.azurewebsites.net/api/authentication/register",
+  //       {
+  //         fullName: fullName,
+  //         email: email,
+  //         password: password,
+  //         confirmPassword: confirmPassword,
+  //       }
+  //     );
+  //     toast.success("Registration successful!");
+  //     setMessage("SIGN UP SUCCESSFULLY - LOG IN NOW");
+  //     setMessageType("success");
+  //     setIsLogin(true);
+  //   } catch (error) {
+  //     if (error.response) {
+  //       toast.error(error.response.data.message || "Registration failed");
+  //     } else if (error.request) {
+  //       toast.error("No response from server");
+  //     } else {
+  //       toast.error(error.message);
+  //     }
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
 
   const loginGoogle = async (response) => {
     var token = response.credential;
@@ -207,7 +205,7 @@ const Login = () => {
           role: decode.role,
         };
         login(userData);
-        navigate(ROUTERS.USER.HOME);
+        // navigate(ROUTERS.USER.HOME);
       } else {
         console.error("Backend error:", data);
         toast.error("Login Failed");
@@ -251,7 +249,7 @@ const Login = () => {
           email: decode.email,
         };
         login(userData);
-        navigate(ROUTERS.USER.HOME);
+        // navigate(ROUTERS.USER.HOME);
       } else {
         console.error("Backend error:", data);
         toast.error("Login Failed");
@@ -274,7 +272,7 @@ const Login = () => {
           {isLogin && (
             <div className="bg-white flex items-center justify-center flex-col px-10 h-full">
               <div className="flex flex-col items-center w-full">
-                <h1 className="text-2xl font-bold mb-6">LOG IN</h1>
+                <h1 className="text-2xl font-bold mb-6 text-black">LOG IN</h1>
 
                 <div className="mb-5 w-full">
                   <GoogleLogin
@@ -286,7 +284,7 @@ const Login = () => {
                   <button
                     type="button"
                     onClick={loginFacebook}
-                    className="flex items-center justify-center bg-blue-600 text-white rounded-md px-4 py-2 w-full hover:bg-blue-700 transition-colors"
+                    className="mt-4 flex items-center justify-center bg-blue-600 text-white rounded-md px-4 py-2 w-full hover:bg-blue-700 transition-colors"
                   >
                     <FaFacebookF className="mr-2" />
                     Continue with Facebook
@@ -303,7 +301,7 @@ const Login = () => {
                   placeholder="Email"
                   onChange={(e) => setEmail(e.target.value)}
                   required
-                  className={`w-full bg-gray-100 border-none rounded-lg px-4 py-3 text-sm mb-2 outline-none ${
+                  className={`w-full bg-gray-100 text-black border-none rounded-lg px-4 py-3 text-sm mb-2 outline-none ${
                     !emailValidation.isValid ? "shadow-red-500 shadow-md" : ""
                   }`}
                 />
@@ -319,7 +317,7 @@ const Login = () => {
                   placeholder="Password"
                   onChange={(e) => setPassword(e.target.value)}
                   required
-                  className={`w-full bg-gray-100 border-none rounded-lg px-4 py-3 text-sm mb-2 outline-none ${
+                  className={`w-full bg-gray-100 text-black border-none rounded-lg px-4 py-3 text-sm mb-2 outline-none ${
                     !passwordValidation.isValid
                       ? "shadow-red-500 shadow-md"
                       : ""
@@ -332,11 +330,11 @@ const Login = () => {
                 )}
 
                 <div className="mb-4">
-                  <Link to="/forget-password">Forgot Password?</Link>
+                  <a to="/forget-password">Forgot Password?</a>
                 </div>
 
                 <button
-                  onClick={handleLogin}
+                  // onClick={handleLogin}
                   disabled={loading}
                   className="bg-green-600 text-white text-xs font-semibold uppercase tracking-wider py-3 px-11 rounded-lg border border-transparent hover:bg-green-700 transition-colors disabled:opacity-50"
                 >
@@ -366,7 +364,7 @@ const Login = () => {
           {!isLogin && (
             <div className="bg-white flex items-center justify-center flex-col px-10 h-full">
               <div className="flex flex-col items-center w-full">
-                <h1 className="text-2xl font-bold mb-6">Create Account</h1>
+                <h1 className="text-2xl font-bold mb-6 text-black">Create Account</h1>
 
                 <span className="text-sm text-gray-600 mb-4">
                   or use your email for registration
@@ -378,7 +376,7 @@ const Login = () => {
                   placeholder="Full Name"
                   onChange={(e) => setFullName(e.target.value)}
                   required
-                  className={`w-full bg-gray-100 border-none rounded-lg px-4 py-3 text-sm mb-2 outline-none ${
+                  className={`w-full bg-gray-100 text-black border-none rounded-lg px-4 py-3 text-sm mb-2 outline-none ${
                     !fullNameValidation.isValid
                       ? "shadow-red-500 shadow-md"
                       : ""
@@ -396,7 +394,7 @@ const Login = () => {
                   placeholder="Email"
                   onChange={(e) => setEmail(e.target.value)}
                   required
-                  className={`w-full bg-gray-100 border-none rounded-lg px-4 py-3 text-sm mb-2 outline-none ${
+                  className={`w-full bg-gray-100 text-black border-none rounded-lg px-4 py-3 text-sm mb-2 outline-none ${
                     !emailValidation.isValid ? "shadow-red-500 shadow-md" : ""
                   }`}
                 />
@@ -412,7 +410,7 @@ const Login = () => {
                   placeholder="Password"
                   onChange={(e) => setPassword(e.target.value)}
                   required
-                  className={`w-full bg-gray-100 border-none rounded-lg px-4 py-3 text-sm mb-2 outline-none ${
+                  className={`w-full bg-gray-100 text-black border-none rounded-lg px-4 py-3 text-sm mb-2 outline-none ${
                     !passwordValidation.isValid
                       ? "shadow-red-500 shadow-md"
                       : ""
@@ -430,7 +428,7 @@ const Login = () => {
                   placeholder="Confirm Password"
                   onChange={(e) => setConfirmPassword(e.target.value)}
                   required
-                  className={`w-full bg-gray-100 border-none rounded-lg px-4 py-3 text-sm mb-2 outline-none ${
+                  className={`w-full bg-gray-100 text-black border-none rounded-lg px-4 py-3 text-sm mb-2 outline-none ${
                     !confirmPasswordValidation.isValid
                       ? "shadow-red-500 shadow-md"
                       : ""
@@ -443,7 +441,7 @@ const Login = () => {
                 )}
 
                 <button
-                  onClick={handleRegister}
+                  // onClick={handleRegister}
                   disabled={loading}
                   className="bg-blue-500 text-white text-xs font-semibold uppercase tracking-wider py-3 px-11 rounded-lg border border-transparent hover:bg-blue-600 transition-colors disabled:opacity-50"
                 >
